1790d9b387a3154cb3ebeb616ca08777e960cfa1
Jeongsiwook <jsu0638@naver.com>
2020-10-09 15:27:55 +0900

    Add files via upload

---
+++ 1427.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main()
+{
+	int i, j, temp;
+	int num, cnt = 0;
+
+	cin >> num;
+	int tmp = num;
+
+	while (tmp != 0) {
+		tmp /= 10;
+		cnt++;
+	}
+
+	int* arr = new int[cnt];
+
+	do {
+		for (i = 0; i < cnt; i++) {
+			arr[i] = num % 10;
+			num /= 10;
+		}		
+	} while (num != 0);
+	
+	for (i = 0; i < cnt - 1; i++) {
+		j = i;
+		while (j >= 0 && arr[j] < arr[j + 1]) {
+			temp = arr[j];
+			arr[j] = arr[j + 1];
+			arr[j + 1] = temp;
+			j--;
+		}
+	}
+
+	for (i = 0; i < cnt; i++) {
+		cout << arr[i];
+	}
+	
+	return 0;
+}
