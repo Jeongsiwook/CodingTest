4b0fe3c222551b39b5a62f007f95876f64a04c81
siwook <jsu0638@naver.com>
2021-12-29 14:41:42 +0900

    해결

--- BinarySearch/부품_찾기.py
+++ BinarySearch/부품_찾기.py
@@ -10,3 +10,56 @@ N = 5
 M = 3
 [5, 7, 9]
 """
+
+# bisect library 사용
+from bisect import bisect_left, bisect_right
+
+N = int(input())
+data = list(map(int, input().split()))
+data.sort()
+M = int(input())
+request = list(map(int, input().split()))
+
+for i in request:
+  if bisect_right(data, i) - bisect_left(data, i):
+    print("yes", end=" ")
+  else:
+    print("no", end=" ")
+    
+
+# binary_search 사용
+def binary_search(array, target, start, end):
+  while start <= end:
+    mid = (start + end) // 2
+    if array[mid] == target:
+      return mid
+    elif array[mid] < target:
+      start = mid + 1
+    else:
+      end = mid - 1
+  return None
+
+n = int(input())
+array = list(map(int, input().split()))
+array.sort()
+m = int(input())
+request = list(map(int, input().split()))
+
+for i in request:
+  if binary_search(array, i, 0, n - 1) != None:
+    print("yes", end=" ")
+  else:
+    print("no", end=" ")
+    
+
+# 집합 자료형 이용
+n = int(input())
+array = set(map(int, input().split()))
+m = int(input())
+x = list(map(int, input().split()))
+
+for i in x:
+  if i in array:
+    print("yes", end=" ")
+  else:
+    print("no", end=" ")
