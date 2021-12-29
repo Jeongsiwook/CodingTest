fe0c9faecc5ec60f02f713a9c3f7afb45e990306
siwook <jsu0638@naver.com>
2021-12-29 22:38:17 +0900

    Create Sort.py

---
+++ Sort/Sort.py
@@ -0,0 +1,10 @@
+# 선택 정렬
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+length = len(array)
+
+for i in range(length):
+  min_index = i
+  for j in range(i + 1, length):
+    if array[min_index] > array[j]:
+      min_index = j
+  array[min_index], array[i] = array[i], array[min_index]
