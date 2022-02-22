42bf89845d5f4a7fe481784612cf40f8d961f2c9
siwook <jsu0638@naver.com>
2022-02-22 22:25:04 +0900

    Update 소수의_판별.py

--- 기타_알고리즘/소수의_판별.py
+++ 기타_알고리즘/소수의_판별.py
@@ -6,7 +6,7 @@
 import math
 
 def is_prime_number(x):
-  for i in rnage(2, int(math.sqrt(x)) + 1):
+  for i in range(2, int(math.sqrt(x)) + 1):
     if x % i == 0:
       return False
   return True
